80bd36fc80c692e4aa8a1fed2c0770b99f7c2fc0
Abdullah <abdullahosama15@gmail.com>
2022-10-23 03:20:33 +0200

    last

--- des.cpp
+++ des.cpp
@@ -8,6 +8,8 @@ using std::chrono::high_resolution_clock;
     using std::chrono::duration;
     using std::chrono::milliseconds;
 
+# define __rdtsc __builtin_ia32_rdtsc
+
 void print_hex(char test[], uint64_t binary) {
     
     printf("This is `%s`: %lX\n", test, binary);
@@ -63,54 +65,54 @@ int expansion_table [] = {
     28, 29, 30, 31, 32, 1
 };
 
-int s_box [8][4][16] = {
+int s_box [8][4 * 16] = {
     {
-        {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
-        {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
-        {4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
-        {15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}
+        14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
+        0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
+        4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
+        15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13
     },
     {
-        {15,  1,   8, 14,   6, 11 ,  3,  4 ,  9,  7 ,  2, 13  ,12,  0,   5, 10},
-        {3 ,13 ,  4 , 7 , 15 , 2  , 8 ,14  ,12 , 0  , 1 ,10   ,6 , 9 , 11 , 5},
-        {0 ,14 ,  7 ,11 , 10 , 4  ,13 , 1  , 5 , 8  ,12 , 6   ,9 , 3 ,  2 ,15},
-        {13,  8,  10,  1,   3, 15 ,  4,  2 , 11,  6 ,  7, 12  , 0,  5,  14,  9}
+        15,  1,   8, 14,   6, 11 ,  3,  4 ,  9,  7 ,  2, 13  ,12,  0,   5, 10,
+        3 ,13 ,  4 , 7 , 15 , 2  , 8 ,14  ,12 , 0  , 1 ,10   ,6 , 9 , 11 , 5,
+        0 ,14 ,  7 ,11 , 10 , 4  ,13 , 1  , 5 , 8  ,12 , 6   ,9 , 3 ,  2 ,15,
+        13,  8,  10,  1,   3, 15 ,  4,  2 , 11,  6 ,  7, 12  , 0,  5,  14,  9
     },
     {
-        {10,  0,   9, 14,   6,  3,  15,  5,   1, 13,  12,  7 , 11,  4,   2,  8},
-        {13,  7,   0,  9,   3,  4,   6, 10,   2,  8,   5, 14 , 12, 11,  15,  1},
-        {13,  6,   4,  9,   8, 15,   3,  0,  11,  1,   2, 12 ,  5, 10,  14,  7},
-        {1 ,10 , 13 , 0 ,  6 , 9 ,  8 , 7 ,  4 ,15 , 14 , 3  ,11 , 5 ,  2 ,12}
+        10,  0,   9, 14,   6,  3,  15,  5,   1, 13,  12,  7 , 11,  4,   2,  8,
+        13,  7,   0,  9,   3,  4,   6, 10,   2,  8,   5, 14 , 12, 11,  15,  1,
+        13,  6,   4,  9,   8, 15,   3,  0,  11,  1,   2, 12 ,  5, 10,  14,  7,
+        1 ,10 , 13 , 0 ,  6 , 9 ,  8 , 7 ,  4 ,15 , 14 , 3  ,11 , 5 ,  2 ,12
     },
     {
-        {7 ,13 , 14 , 3 ,  0 , 6 ,  9 ,10  , 1 , 2 ,  8 , 5 , 11 ,12 ,  4 ,15},
-        {13,  8,  11,  5,   6, 15,   0,  3 ,  4,  7,   2, 12,   1, 10,  14,  9},
-        {10,  6,   9,  0,  12, 11,   7, 13 , 15,  1,   3, 14,   5,  2,   8,  4},
-        {3 ,15 ,  0 , 6 , 10 , 1 , 13 , 8  , 9 , 4 ,  5 ,11 , 12 , 7 ,  2 ,14}
+        7 ,13 , 14 , 3 ,  0 , 6 ,  9 ,10  , 1 , 2 ,  8 , 5 , 11 ,12 ,  4 ,15,
+        13,  8,  11,  5,   6, 15,   0,  3 ,  4,  7,   2, 12,   1, 10,  14,  9,
+        10,  6,   9,  0,  12, 11,   7, 13 , 15,  1,   3, 14,   5,  2,   8,  4,
+        3 ,15 ,  0 , 6 , 10 , 1 , 13 , 8  , 9 , 4 ,  5 ,11 , 12 , 7 ,  2 ,14
     },
     {
-        {2 ,12 ,  4 , 1 ,  7 ,10 , 11 , 6 ,  8 , 5 ,  3 ,15 , 13 , 0 , 14 , 9},
-        {14, 11,   2, 12,   4,  7,  13,  1,   5,  0,  15, 10,   3,  9,   8,  6},
-        {4 , 2 ,  1 ,11 , 10 ,13 ,  7 , 8 , 15 , 9 , 12 , 5 ,  6 , 3 ,  0 ,14},
-        {11,  8,  12,  7,   1, 14,   2, 13,   6, 15,   0,  9,  10,  4,   5,  3}
+        2 ,12 ,  4 , 1 ,  7 ,10 , 11 , 6 ,  8 , 5 ,  3 ,15 , 13 , 0 , 14 , 9,
+        14, 11,   2, 12,   4,  7,  13,  1,   5,  0,  15, 10,   3,  9,   8,  6,
+        4 , 2 ,  1 ,11 , 10 ,13 ,  7 , 8 , 15 , 9 , 12 , 5 ,  6 , 3 ,  0 ,14,
+        11,  8,  12,  7,   1, 14,   2, 13,   6, 15,   0,  9,  10,  4,   5,  3
     },
     {
-        {12,  1,  10, 15,   9,  2,   6,  8,   0, 13,   3,  4,  14,  7,   5, 11},
-        {10, 15,   4,  2,   7, 12,   9,  5,   6,  1,  13, 14,   0, 11,   3,  8},
-        {9 ,14 , 15 , 5 ,  2 , 8 , 12 , 3 ,  7 , 0 ,  4 ,10 ,  1 ,13 , 11 , 6},
-        {4 , 3 ,  2 ,12 ,  9 , 5 , 15 ,10 , 11 ,14 ,  1 , 7 ,  6 , 0 ,  8 ,13}
+        12,  1,  10, 15,   9,  2,   6,  8,   0, 13,   3,  4,  14,  7,   5, 11,
+        10, 15,   4,  2,   7, 12,   9,  5,   6,  1,  13, 14,   0, 11,   3,  8,
+        9 ,14 , 15 , 5 ,  2 , 8 , 12 , 3 ,  7 , 0 ,  4 ,10 ,  1 ,13 , 11 , 6,
+        4 , 3 ,  2 ,12 ,  9 , 5 , 15 ,10 , 11 ,14 ,  1 , 7 ,  6 , 0 ,  8 ,13
     },
     {
-        {4 ,11 ,  2 ,14 , 15 , 0 ,  8 ,13 ,  3 ,12 ,  9 , 7 ,  5 ,10 ,  6 , 1},
-        {13,  0,  11,  7,   4,  9,   1, 10,  14,  3,   5, 12,   2, 15,   8,  6},
-        {1 , 4 , 11 ,13 , 12 , 3 ,  7 ,14 , 10 ,15 ,  6 , 8 ,  0 , 5 ,  9 , 2},
-        {6 ,11 , 13 , 8 ,  1 , 4 , 10 , 7 ,  9 , 5 ,  0 ,15 , 14 , 2 ,  3 ,12}
+        4 ,11 ,  2 ,14 , 15 , 0 ,  8 ,13 ,  3 ,12 ,  9 , 7 ,  5 ,10 ,  6 , 1,
+        13,  0,  11,  7,   4,  9,   1, 10,  14,  3,   5, 12,   2, 15,   8,  6,
+        1 , 4 , 11 ,13 , 12 , 3 ,  7 ,14 , 10 ,15 ,  6 , 8 ,  0 , 5 ,  9 , 2,
+        6 ,11 , 13 , 8 ,  1 , 4 , 10 , 7 ,  9 , 5 ,  0 ,15 , 14 , 2 ,  3 ,12
     },
     {
-        {13,  2,   8,  4,   6, 15,  11,  1,  10,  9,   3, 14,   5,  0,  12,  7},
-        {1 ,15 , 13 , 8 , 10 , 3 ,  7 , 4 , 12 , 5 ,  6 ,11 ,  0 ,14 ,  9 , 2},
-        {7 ,11 ,  4 , 1 ,  9 ,12 , 14 , 2 ,  0 , 6 , 10 ,13 , 15 , 3 ,  5 , 8},
-        {2 , 1 , 14 , 7 ,  4 ,10 ,  8 ,13 , 15 ,12 ,  9 , 0 ,  3 , 5 ,  6 ,11},
+        13,  2,   8,  4,   6, 15,  11,  1,  10,  9,   3, 14,   5,  0,  12,  7,
+        1 ,15 , 13 , 8 , 10 , 3 ,  7 , 4 , 12 , 5 ,  6 ,11 ,  0 ,14 ,  9 , 2,
+        7 ,11 ,  4 , 1 ,  9 ,12 , 14 , 2 ,  0 , 6 , 10 ,13 , 15 , 3 ,  5 , 8,
+        2 , 1 , 14 , 7 ,  4 ,10 ,  8 ,13 , 15 ,12 ,  9 , 0 ,  3 , 5 ,  6 ,11,
     }
 };
 
@@ -140,17 +142,17 @@ uint64_t keys [16];
 
 uint64_t ONE = 1;
 
-uint64_t circular_shift (uint64_t target, int shifts, int len) {
+uint64_t circular_shift (uint64_t& target, int shifts, int len) {
     return ((target << shifts) | (target >> len - shifts)) & ((ONE << 28) - 1);
 }
 
 
 
-void generate_keys(uint64_t key) {
+void generate_keys(uint64_t& key) {
 
     uint64_t permuted_out = 0;
     for(int i = 0; i < 56; i++) {
-        permuted_out |=  (((key >> (64 - permuted_choice_1[i]) ) & ONE) << (55 - i));
+        permuted_out =  (((key >> (64 - permuted_choice_1[i]) ) & ONE) | (permuted_out << 1));
     }
     // print_hex("Permuted key 1", permuted_out);
 
@@ -170,35 +172,41 @@ void generate_keys(uint64_t key) {
         // printf("Concat no %i: %lX\n", i, concat_keys);
         uint64_t final_key = 0;
         for(int j = 0; j < 48; j++) {
-            final_key |= ((( concat_keys >> (56 - permuted_choice_2[j]) ) & ONE) << (47 - j));
+            final_key = ((( concat_keys >> (56 - permuted_choice_2[j]) ) & ONE) | (final_key << 1));
         }
         // printf("Final Key %i: %lX\n", i, final_key);
         keys[i] = final_key;
     }
 }
-
-uint64_t feistel_cipher (uint64_t in, uint64_t key) {
+long long tot_cycles = 0;
+long long count = 0;
+uint64_t feistel_cipher (uint64_t& in, uint64_t& key) {
     uint64_t expan_in = 0;
     for(int i = 0; i < 48; ++i) {
-        expan_in |= (((in >> 32 - expansion_table[i]) & ONE) << (47 - i));
+        expan_in = (((in >> 32 - expansion_table[i]) & ONE) | (expan_in << 1));
     }
 
     // print_hex("right", in);
     expan_in ^= key;
     // print_hex("Expansion", expan_in);
+    long long t1=__rdtsc();
+
     uint32_t s_box_output = 0;
     for(int i = 0; i < 8; ++i) {
         uint8_t part = (expan_in >> (42 - i * 6)) & 0x3F;
         uint8_t row = (((part & 0x20) >> 4) | (part & 1));
         uint8_t col = (part >> 1) & 0xF;
-        s_box_output |= ( (uint64_t)s_box[i][row][col] << (28 - i * 4));
+        s_box_output |= ( (uint64_t)s_box[i][(row << 4) | col] << (28 - i * 4));
         // printf("row %lX, col %lX, part %lX\n", row, col, part);
     }
+    long long t2=__rdtsc();
+    count++;
+    tot_cycles += t2 - t1;
     // print_hex("s-box ", s_box_output);
     
     uint32_t out = 0;
     for(int i = 0; i < 32; ++i) {
-        out |= (((s_box_output >> 32 - f_permutaion[i]) & ONE) << (31 - i));
+        out = (((s_box_output >> 32 - f_permutaion[i]) & ONE) | (out << 1));
     }
 
     return out;
@@ -207,14 +215,12 @@ uint64_t feistel_cipher (uint64_t in, uint64_t key) {
 uint64_t mutate_data (uint64_t data, bool encrypt) {
     uint64_t permuted_data = 0;
     for(int i = 0; i < 64; ++i) {
-        permuted_data |= (((data >> 64 - initial_permutation[i]) & ONE) << (63 - i));
+        permuted_data = (((data >> 64 - initial_permutation[i]) & ONE) | (permuted_data << 1));
     }
 
     uint64_t left = permuted_data >> 32;
     uint64_t right = permuted_data & ((ONE << 32) - 1);
-    // print_hex("Permu data", permuted_data);
-    // print_hex("Permu data left", left);
-    // print_hex("Permu data right", right);
+
 
     for(int i = 0; i < 16; ++i) {
         uint64_t temp = right;
@@ -222,11 +228,10 @@ uint64_t mutate_data (uint64_t data, bool encrypt) {
         right = feistel_cipher(right, keys[key]) ^ left;
         left = temp;
     }
-    // printf("Final left %lX\n right %lX\n", left, right);
     uint64_t swapped = (right << 32) | left;  
     uint64_t final_out = 0;
     for(int i = 0; i < 64; ++i) {
-        final_out |= (((swapped >> 64 - inverse_permutation[i]) & ONE) << (63 - i));
+        final_out = (((swapped >> 64 - inverse_permutation[i]) & ONE) | (final_out << 1));
     }
     return final_out;
 }  
@@ -361,6 +366,6 @@ int main(int argc, char* argv[]) {
     startup2(data, key, (isEncrypt) ? encrypted_out : decrypted_out, hex_out, isEncrypt);
     auto t2 = high_resolution_clock::now();
     duration<double, std::milli> ms_double = t2 - t1;
-
+    printf("Total cycles %lld %f\n", tot_cycles, tot_cycles*1.0 / count);
     printf("Time taken %f\n", ms_double.count());
 }
